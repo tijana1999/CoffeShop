4ab1a3aba1fa1a2251f25936ca6677282cb723e0
tijana1999 <60898526+tijana1999@users.noreply.github.com>
2020-02-11 00:19:09 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -119,19 +119,13 @@ window.addEventListener("load",function(){
       }
 
       $(document).ready(function(){
-        $('#menu li ul').css({
-          display: "none",
-          left: "auto"
-        });
         $('#menu li').hover(function() {
           $(this)
             .find('ul')
-            .stop(true, true)
-            .slideDown('fast');
+           .slideDown('fast');
         }, function() {
           $(this)
             .find('ul')
-            .stop(true,true)
             .fadeOut('fast');
         });
       });
